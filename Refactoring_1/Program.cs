aa877dd24cf6f1ed541cb1f08b2111d93a084b32
martin-newbie <g.martin.min@gmail.com>
2022-10-01 16:24:11 +0900

    refactoring_1
    switch문 순수 함수 추출, 변수 이름 수정, 변수 인라인화

--- Refactoring_1/Program.cs
+++ Refactoring_1/Program.cs
@@ -16,43 +16,54 @@ namespace Refactoring_1
             var totalAmount = 0;
             var volumeCredits = 0;
             var result = $"청구 내역 {invoice.customer}\n";
+            
+            Play PlayFor(Performances performance)
+            {
+                return plays[performance.playID];
+            }
 
             foreach (var perf in invoice.performances)
             {
-                var play = plays[perf.playID];
                 var thisAmount = 0;
-
-                switch (play.type)
-                {
-                    case PlayType.tragedy:
-                        thisAmount = 40000;
-                        if (perf.audience > 30)
-                        {
-                            thisAmount += 1000 * (perf.audience - 30);
-                        }
-                        break;
-                    case PlayType.comedy:
-                        thisAmount = 30000;
-                        if (perf.audience > 20)
-                        {
-                            thisAmount += 10000 + 500 * (perf.audience - 20);
-                        }
-                        thisAmount += 300 * perf.audience;
-                        break;
-                    default:
-                        throw new Exception($"알 수 없는 장르: {play.type}");
-                }
+                thisAmount = AmountFor(perf, PlayFor(perf));
 
                 volumeCredits += Math.Max(perf.audience - 30, 0);
 
-                if (play.type == PlayType.comedy) volumeCredits += (int)MathF.Floor(perf.audience / 5);
+                if (PlayFor(perf).type == PlayType.comedy) volumeCredits += (int)MathF.Floor(perf.audience / 5);
 
-                result += $"{play.name}: {format(thisAmount / 100)} ({perf.audience}석)\n";
+                result += $"{PlayFor(perf).name}: {format(thisAmount / 100)} ({perf.audience}석)\n";
                 totalAmount += thisAmount;
             }
 
             result += $"총액: {format(totalAmount / 100)}\n";
             result += $"적립 포인트: {volumeCredits}점\n";
+            return result;
+        }
+
+        private static int AmountFor(Performances performance, Play play)
+        {
+            int result;
+            switch (play.type)
+            {
+                case PlayType.tragedy:
+                    result = 40000;
+                    if (performance.audience > 30)
+                    {
+                        result += 1000 * (performance.audience - 30);
+                    }
+                    break;
+                case PlayType.comedy:
+                    result = 30000;
+                    if (performance.audience > 20)
+                    {
+                        result += 10000 + 500 * (performance.audience - 20);
+                    }
+                    result += 300 * performance.audience;
+                    break;
+                default:
+                    throw new Exception($"알 수 없는 장르: {play.type}");
+            }
+
             return result;
         }
 
