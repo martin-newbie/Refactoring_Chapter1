cb02c39b82d6a8c8a9056e6ccf6da0554fd39cd9
martin-newbie <g.martin.min@gmail.com>
2022-10-02 15:45:15 +0900

    Refactoring_9
    파일 나누기

--- Refactoring_1/Program.cs
+++ Refactoring_1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Refactoring_1
 {
@@ -14,68 +13,9 @@ namespace Refactoring_1
     {
         public static string Statement(Invoice invoice, Dictionary<string, Play> plays)
         {
-            Invoice statementData = new Invoice();
 
-            statementData.customer = invoice.customer;
-            statementData.performances = invoice.performances.Select(EnrichPerformance).ToList();
-            statementData.totalAmount = TotalAmount(statementData);
-            statementData.totalVolumeCredits = TotalVolumeCredit(statementData);
+            return RenderPlainText(CreateStatement.CreateStatementData(invoice, plays));
 
-            return RenderPlainText(statementData);
-
-            Performances EnrichPerformance(Performances performances)
-            {
-                var result = performances.Clone();
-                result.play = PlayFor(result);
-                result.amount = AmountFor(result);
-                result.volumeCredits = VolumeCreditFor(result);
-                return result;
-            }
-            Play PlayFor(Performances performance)
-            {
-                return plays[performance.playID];
-            }
-            int AmountFor(Performances performance)
-            {
-                int result;
-                switch (PlayFor(performance).type)
-                {
-                    case PlayType.tragedy:
-                        result = 40000;
-                        if (performance.audience > 30)
-                        {
-                            result += 1000 * (performance.audience - 30);
-                        }
-                        break;
-                    case PlayType.comedy:
-                        result = 30000;
-                        if (performance.audience > 20)
-                        {
-                            result += 10000 + 500 * (performance.audience - 20);
-                        }
-                        result += 300 * performance.audience;
-                        break;
-                    default:
-                        throw new Exception($"알 수 없는 장르: {PlayFor(performance).type}");
-                }
-
-                return result;
-            }
-            int VolumeCreditFor(Performances performance)
-            {
-                var result = Math.Max(performance.audience - 30, 0);
-
-                if (performance.play.type == PlayType.comedy) result += (int)MathF.Floor(performance.audience / 5);
-                return result;
-            }
-            int TotalAmount(Invoice data)
-            {
-                return data.performances.Aggregate(0, (total, p) => total + p.amount);
-            }
-            int TotalVolumeCredit(Invoice data)
-            {
-                return data.performances.Aggregate(0, (total, p) => total + p.volumeCredits);
-            }
         }
 
         static string RenderPlainText(Invoice data)
