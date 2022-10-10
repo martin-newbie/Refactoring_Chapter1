995d5ce685f2f4761307ad52369bde83c30cc45f
martin_newbie <g.martin.min@gmail.com>
2022-10-10 19:54:52 +0900

    Test
    add test code

---
+++ TestProject/InvoiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Refactoring_1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactoring_1.Tests
+{
+    [TestClass()]
+    public class InvoiceTests
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+            // Tests that we expect to return true.
+            string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+            foreach (var word in words)
+            {
+                bool result = true;
+                Assert.IsTrue(result,
+                              $"Expected for '{word}': true; Actual: {result}");
+            }
+        }
+
+        [TestMethod()]
+        public void InvoiceTest()
+        {
+            var invoice = new Invoice("user1", new List<Performances>() { new Performances("id1", 50) });
+            Assert.IsTrue(invoice != null, "error");
+
+            //Assert.Fail();
+        }
+    }
+}
